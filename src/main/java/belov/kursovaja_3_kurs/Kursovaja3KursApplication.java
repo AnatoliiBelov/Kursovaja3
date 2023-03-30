6caae1f30d77df8cf162ba7d1b90caf762c3e8c9
AnatoliiBelov <anatoly.belov.ne@gmail.om>
2023-03-30 11:58:17 +0300

    Старт

---
+++ src/main/java/belov/kursovaja_3_kurs/Kursovaja3KursApplication.java
@@ -0,0 +1,13 @@
+package belov.kursovaja_3_kurs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kursovaja3KursApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Kursovaja3KursApplication.class, args);
+    }
+
+}
---
+++ src/test/java/belov/kursovaja_3_kurs/Kursovaja3KursApplicationTests.java
@@ -0,0 +1,13 @@
+package belov.kursovaja_3_kurs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kursovaja3KursApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
